10ff128fec3e13141d879defabac43cf1b05dc54
paw3l-syp3k <157652023+paw3l-syp3k@users.noreply.github.com>
2025-02-19 15:30:48 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,3 +14,28 @@ menuItems.forEach(item => {
         menuList.classList.toggle('nav-active')
     })
 })
+
+
+
+
+function sendMail() {
+        let name = document.getElementById('name').value;
+        let surname = document.getElementById('surname').value;
+        let company= document.getElementById('company').value;
+        let phone= document.getElementById('phone').value;
+        let email= document.getElementById('email').value;
+        let msg= document.getElementById('msg').value;
+    let parms = {
+        name: name,
+        surname: surname,
+        company: company,
+        phone: phone,
+        email: email,
+        msg: msg,
+    }
+   
+     emailjs.send('service_3nlzem9','template_i47c6g7',parms).then(alert('Email geschickt!'))
+
+
+    
+}
